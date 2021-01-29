e1b2a442dab10523bff481c2339d046dfcb04109
Luis Ricardo Teixeira Fernandes <lrtf.developer@gmail.com>
2021-01-29 12:15:57 -0300

    T - classes de teste de Produto

--- src/main/java/br/com/pulse/dto/ProdutoDto.java
+++
@@ -1,5 +0,0 @@
-package br.com.pulse.dto;
-
-public class ProdutoDto {
-
-}
--- src/test/java/br/com/pulse/AppTests.java
+++ src/test/java/br/com/pulse/AppTests.java
@@ -1,14 +1,9 @@
 package br.com.pulse;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Random;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class AppTests {
 
   @Test
